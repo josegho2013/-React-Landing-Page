390636fca0ee9e8e7a5c9fdc22bfce3cb54fc463
jose hernandez <josegho2013@gmail.com>
2022-03-10 20:41:09 -0300

    styles navbar

--- landing-page/src/components/Navbar.js
+++ landing-page/src/components/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import "../styles/navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar_container'
+    
+    >Navbar</div>
   )
 }
 
