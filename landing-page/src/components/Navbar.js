4c0c41a821f0f06bc682b1a01788954bc9db07a3
jose hernandez <josegho2013@gmail.com>
2022-03-25 16:18:30 -0300

    update

--- landing-page/src/components/Navbar.js
+++ landing-page/src/components/Navbar.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
-import { GiGecko } from "react-icons/gi";
+
+import { VscCode } from "react-icons/vsc";
+import { VscFileCode } from "react-icons/vsc";
+
 //import { Nav, NavItem, NavLink, Jumbotron, Button } from "reactstrap";
 import "../styles/navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar_container">
+      <VscFileCode size="4rem" color="white"/>
       <h1>
-        {" "}
-        <GiGecko size="4rem" />
         MANGO <br />
         DEV
       </h1>
       <div className="links">
-        <Link to="/Home">
-          <button>Home</button>
-        </Link>
+        <button>Home</button>
+        <button>About</button>
+        <button>Skill</button>
+        <button>Works</button>
       </div>
+      <VscCode size="4rem" color="white" />;
     </div>
   );
 };
--- landing-page/src/components/Title.js
+++ landing-page/src/components/Title.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import "../styles/title.css"
+import React from "react";
+import "../styles/title.css";
 
 const Title = () => {
   return (
-    <div >
-      
-      
-      </div>
-  )
-}
+    <div>
+      <img  className="imagen"   src="https://static.wixstatic.com/media/669128_ec1c7a78e9694aec8a07c2e48b292ae1~mv2.gif" />
+    </div>
+  );
+};
 
-export default Title
+export default Title;
