e05b05e22d66b39f6a09d31734448cc834a66d43
jose hernandez <josegho2013@gmail.com>
2022-03-25 13:00:34 -0300

    update

--- landing-page/src/components/Navbar.js
+++ landing-page/src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/navbar.css";
 
+
 const Navbar = () => {
   return (
     <div className="navbar_container">
-      <h1>soy la navbar</h1>
+      <nav>
+
+
+      </nav>
     </div>
   );
 };
