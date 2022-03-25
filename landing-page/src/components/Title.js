c87db82b86c8088e03664de51cfa793d84a85f24
jose hernandez <josegho2013@gmail.com>
2022-03-25 19:49:11 -0300

    styles

--- landing-page/src/components/Title.js
+++ landing-page/src/components/Title.js
@@ -4,10 +4,13 @@ import "../styles/title.css";
 const Title = () => {
   return (
     <div>
+      <div className="container">
       <img
         className="imagen"
         src="https://static.wixstatic.com/media/669128_ec1c7a78e9694aec8a07c2e48b292ae1~mv2.gif"
       />
+
+       </div>
     </div>
   );
 };
