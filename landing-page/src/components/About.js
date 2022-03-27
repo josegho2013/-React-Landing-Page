83150e0a6bc8aa28e52bc54a50d42c104fcffea7
jose hernandez <josegho2013@gmail.com>
2022-03-27 13:03:20 -0300

    styles about

--- landing-page/src/components/About.js
+++ landing-page/src/components/About.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import "../styles/about.css"
+import React from "react";
+import "../styles/about.css";
 
 const About = () => {
   return (
-    <div className='about-container'>
-
-
+    <div className="about-container">
+      <img
+        className="imagen-about"
+        src="https://blog.depositphotos.com/wp-content/uploads/2019/04/Depositphotos_209179682_m-2015kk.jpg.webp"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
