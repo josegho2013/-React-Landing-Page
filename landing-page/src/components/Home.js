cea03e60e91b6dc1eaacc0da175bb3eb06e228ef
jose hernandez <josegho2013@gmail.com>
2022-03-27 12:11:50 -0300

    optimizando componentes

--- landing-page/src/components/Home.js
+++ landing-page/src/components/Home.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
-import Landing from "./Landing";
-// import Clients from "./Clients";
-
 import "../styles/home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <Navbar />
-      <Landing />
+
+      <div>
+        <img
+          className="imagen"
+          src="https://static.wixstatic.com/media/669128_ec1c7a78e9694aec8a07c2e48b292ae1~mv2.gif"
+        />
+      </div>
 
       <Footer />
     </div>
