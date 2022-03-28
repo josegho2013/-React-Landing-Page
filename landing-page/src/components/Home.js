24338e16fc0a276859ab8c6878814a1408fe1b70
jose hernandez <josegho2013@gmail.com>
2022-03-28 09:33:03 -0300

    styles home

--- landing-page/src/components/Home.js
+++ landing-page/src/components/Home.js
@@ -5,11 +5,14 @@ import "../styles/home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <div>
+      <div className="left-image">
         <img
           className="imagen"
           src="https://static.wixstatic.com/media/669128_ec1c7a78e9694aec8a07c2e48b292ae1~mv2.gif"
         />
+      </div>
+      <div className="right-image">
+        <h1>holaaaa aqui va mi foto</h1>
       </div>
     </div>
   );
