861635d24755b36adfb1e2246c5ffa08816195c4
jose hernandez <josegho2013@gmail.com>
2022-03-28 11:17:21 -0300

    .

--- landing-page/src/components/Skill.js
+++ landing-page/src/components/Skill.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Button } from "reactstrap";
-import "../styles/skill.css"
+import "../styles/skill.css";
 
 const Skill = () => {
   return (
     <div className="container-skill">
       <div className="left-skill">
-        <Button>FRONT END</Button>
+        <Button color="warning">FRONT END</Button>
+        <div className="img-logos">
+          <div className="img01"></div>
+          <div className="img02"></div>
+          <div className="img03"></div>
+          <div className="img04"></div>
+          <div className="img05"></div>
+        </div>
       </div>
       <div className="right-skill">
-        <Button>BACKEND</Button>
+        <Button color="warning" center-block>
+          BACKEND
+        </Button>
       </div>
     </div>
   );
