b700a5dd1f22b13c09fe27cf9ea1bfdd9191288e
jose hernandez <josegho2013@gmail.com>
2022-03-28 12:28:08 -0300

    update

--- landing-page/src/components/Skill.js
+++ landing-page/src/components/Skill.js
@@ -7,18 +7,25 @@ const Skill = () => {
     <div className="container-skill">
       <div className="left-skill">
         <Button color="warning">FRONT END</Button>
-        <div className="img-logos">
+        <div className="img-logos01">
           <div className="img01"></div>
           <div className="img02"></div>
           <div className="img03"></div>
-          <div className="img04"></div>
+        <div className="img04"></div>
           <div className="img05"></div>
+          <div className="img06"></div>
         </div>
       </div>
       <div className="right-skill">
-        <Button color="warning" center-block>
-          BACKEND
-        </Button>
+        <Button color="warning">BACKEND</Button>
+        <div className="img-logos02">
+          <div className="img07"></div>
+          <div className="img08"></div>
+          <div className="img09"></div>
+          <div className="img10"></div>
+          <div className="img11"></div>
+          <div className="img12"></div>
+        </div>
       </div>
     </div>
   );
